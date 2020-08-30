4118c9e21376a4ca4f125ec12e3a372723ea77fe
yike <yikel@seas.upenn.edu>
2020-08-29 20:55:42 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Yike Li on 8/29/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
